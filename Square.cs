a44cc4351359f68eba37739076b0ccdaa91dfdc7
NexusGeniuz <nexusgeniuz@gmail.com>
2016-10-30 17:14:23 +0400

    v0.1

---
+++ Square.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Square : MonoBehaviour
+{
+    [HideInInspector]
+    public int x;
+    [HideInInspector]
+    public int y;
+
+    public bool isOpened = false;
+    public bool isBomb;
+    public bool isFlagged = false;
+    public int bombsAround;
+    public Text ba;
+    public Image isBombI;
+    public Image isFlaggedI;
+
+    Ray ray;
+    RaycastHit hit;
+    Color generalColor;
+
+    void Start()
+    {
+        generalColor = transform.GetComponent<Renderer>().material.color;
+    }
+
+    public void SetValues()
+    {
+        if(isBomb)
+        {
+            isBombI.gameObject.SetActive(true);
+            return;
+        }
+
+        ba.text = bombsAround > 0 ? bombsAround.ToString() : "";
+        if (bombsAround == 1)
+            ba.color = Color.blue;
+        else if (bombsAround == 2)
+            ba.color = Color.green;
+        else if (bombsAround >= 3)
+            ba.color = Color.red;
+    }
+
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                if(hit.collider.gameObject == gameObject)
+                {
+                    SetFlag();
+                }
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+        Open();
+    }
+
+    public void Open()
+    {
+        if (!isOpened && !isFlagged)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            gameObject.GetComponent<Renderer>().material.color = Color.white;
+            isOpened = true;
+            SquareManager.OpenSquares(x, y);
+
+            if (isBomb)
+            {
+                print("Game over!");
+                gameObject.GetComponent<Renderer>().material.color = Color.red;
+                isBombI.gameObject.SetActive(true);
+                return;
+                //TODO ------------------------------
+            }
+        }
+    }
+
+    void SetFlag()
+    {
+        if (isOpened)
+            return;
+
+        transform.GetChild(0).gameObject.SetActive(!transform.GetChild(0).gameObject.activeSelf);
+        transform.GetChild(0).GetChild(0).gameObject.SetActive(!transform.GetChild(0).GetChild(0).gameObject.activeSelf);
+        isBombI.gameObject.SetActive(false);
+        //isFlaggedI.gameObject.SetActive(!isFlaggedI.gameObject.activeSelf);
+
+        gameObject.GetComponent<Renderer>().material.color = transform.GetComponent<Renderer>().material.color == generalColor ? new Color(1, 0.5f, 0) : generalColor;
+        isFlagged = !isFlagged;
+    }
+}
---
+++ SquareManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class SquareManager : MonoBehaviour
+{
+    static Square[,] squares;
+    static int maxX, maxY;
+    static LevelGenerator lg;
+
+    void Awake()
+    {
+        squares = null;
+        maxX = 0;
+        maxY = 0;
+        lg = GetComponent<LevelGenerator>();
+    }
+
+    public static void OwnStart()
+    {
+        squares = lg.squares;
+        maxX = squares.GetLength(0);
+        maxY = squares.GetLength(1);
+    }
+
+    public static void OpenSquares(int x, int y)
+    {
+        // Открыть все блоки вокруг, если нажатый блок пустой
+        if (squares[x, y].bombsAround == 0 && !squares[x, y].isBomb)
+        {
+            for (int X = -1; X <= 1; X++)
+            {
+                if (x + X < 0 || x + X >= maxX)
+                    continue;
+
+                for (int Y = -1; Y <= 1; Y++)
+                {
+                    if (y + Y < 0 || y + Y >= maxY || x + X == 0 && y + Y == 0)
+                        continue;
+
+                    squares[x + X, y + Y].Open();
+                }
+            }
+        }
+    }
+}
