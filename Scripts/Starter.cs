be63a9929b2d74157525a5dfb6e9100435147887
NexusGeniuz <nexusgeniuz@gmail.com>
2016-11-10 23:31:46 +0400

    v0.7 some changes

--- Scripts/Starter.cs
+++ Scripts/Starter.cs
@@ -15,8 +15,8 @@ public class Starter : MonoBehaviour
         porter.maxX = 9;
         porter.maxY = 9;
         porter.bombsAmount = 10;
-        porter.cameraOffset = new Vector3(0, 0.7f, -10f);
-        porter.cameraSize = 5.9f;
+        porter.cameraOffset = new Vector3(0, 0.5f, -10f);
+        porter.cameraSize = 5.3f;
         porter.createOnCorners = false;
         LoadLevel();
     }
@@ -27,7 +27,7 @@ public class Starter : MonoBehaviour
         porter.maxY = 12;
         porter.bombsAmount = 30;
         porter.cameraOffset = new Vector3(0, 0.7f, -10f);
-        porter.cameraSize = 7.65f;
+        porter.cameraSize = 7.1f;
         porter.createOnCorners = true;
         LoadLevel();
     }
@@ -37,8 +37,8 @@ public class Starter : MonoBehaviour
         porter.maxX = 16;
         porter.maxY = 16;
         porter.bombsAmount = 69;
-        porter.cameraOffset = new Vector3(0, 1.1f, -10f);
-        porter.cameraSize = 10.35f;
+        porter.cameraOffset = new Vector3(0, 0.9f, -10f);
+        porter.cameraSize = 9.4f;
         porter.createOnCorners = true;
         LoadLevel();
     }
